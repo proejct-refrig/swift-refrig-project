55ab33ce84e2603583a902faa796c6436b74f67b
Rowoon <dev.rowoon@gmail.com>
2025-04-22 17:11:38 +0900

    Initial Commit

---
+++ refrig-project-swift/refrig_project_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  refrig_project_swiftApp.swift
+//  refrig-project-swift
+//
+//  Created by 이로운 on 4/22/25.
+//
+
+import SwiftUI
+
+@main
+struct refrig_project_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
